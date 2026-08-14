0066d81d7eceed06dd7ad10fce4b13bfd7bf20a8
WaTaShi <jm.pasamba5@gmail.com>
2026-08-15 01:26:46 +0800

    Add Vol 18-22 and Complete Collection to PDF downloads

--- js/downloads/controller.js
+++ js/downloads/controller.js
@@ -1,31 +1,19 @@
 app.initDownloads = async function (routerInstance) {
 	let template = await fetch(APP_BASE + "/js/downloads/view.html").then(owo => owo.text())
 
-	let files = [
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 1.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 2.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 3.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 4.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 5.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 6.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 7.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 8.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 9.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 10.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 11.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 12.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 14.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 15.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 16.pdf",
-		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. 17.pdf"
-	]
+	let volumeNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+
+	let files = volumeNumbers.map(vol =>
+		"PDF_Slimes/That Time I Got Reincarnated as a Slime, Vol. " + vol + ".pdf"
+	)
+	files.unshift("PDF_Slimes/Tensei Shitara Slime Datta Ken_ Complete.pdf")
 
 	let pdfs = files.map(file => {
 		let match = file.match(/Vol\. (\d+)\.pdf$/)
 		let vol = match ? Number(match[1]) : 0
 		return {
 			vol,
-			name: match ? "Volume " + match[1] : file,
+			name: match ? "Volume " + match[1] : "Complete Collection (Volumes 1\u201322)",
 			href: (window.APP_BASE || "") + "/" + file.split("/").map(encodeURIComponent).join("/")
 		}
 	}).sort((a, b) => a.vol - b.vol)
